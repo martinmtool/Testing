1c932dd5670bf35bbb49d1ec0729cd314148db0f
Martin Lichstam <martin@mediatool.com>
2018-06-30 23:42:51 +0200

    shit

---
+++ packages/b/src/AppB.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AppB extends Component {
+  render() {
+    return (
+      <div>
+        <p className="shit">
+          To get started, edit <code>src/App.js</code> and save to shit.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AppB;
